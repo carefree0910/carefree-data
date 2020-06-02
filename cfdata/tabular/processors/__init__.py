f8f6c4edffb2e21bff4e3631127d4e1a591b5b00
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-02 16:29:57 +0800

    Create __init__.py

---
+++ cfdata/tabular/processors/__init__.py
@@ -0,0 +1,9 @@
+from .base import *
+from .normalize import Normalize
+from .min_max import MinMax
+from .function import Lambda
+from .one_hot import OneHot
+from .identical import Identical
+
+
+__all__ = ["Processor", "processor_dict"]
