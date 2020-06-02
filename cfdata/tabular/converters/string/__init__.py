c63f1054ad383a1d5b94a7ac0f3153624af8706b
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-02 16:25:38 +0800

    实现了 StringConverter
    注意 recover 之后的数据类型是 np.str

---
+++ cfdata/tabular/converters/string/__init__.py
@@ -0,0 +1,4 @@
+from .core import StringConverter
+
+
+__all__ = ["StringConverter"]
---
+++ cfdata/tabular/converters/string/core.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from ...types import *
+from ..base import Converter
+
+
+@Converter.register("string")
+class StringConverter(Converter):
+    def _fit(self) -> "Converter":
+        assert self.info.is_valid and self.info.is_string and self.info.need_transform
+        self._transform_dict = self._recognizer.transform_dict
+        self._reverse_transform_dict = dict(map(reversed, self._transform_dict.items()))
+        return self
+
+    def _convert(self,
+                 flat_arr: flat_arr_type) -> np.ndarray:
+        return np.array([self._transform_dict[elem] for elem in flat_arr], np.float32)
+
+    def _recover(self,
+                 flat_arr: flat_arr_type) -> np.ndarray:
+        return np.array([self._reverse_transform_dict[elem] for elem in flat_arr], np.str)
+
+
+__all__ = ["StringConverter"]
