0234df28106172e332195c0b2bdcc3e6ca6fd85a
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-02 16:27:50 +0800

    实现了 Identical Processor

---
+++ cfdata/tabular/processors/identical/__init__.py
@@ -0,0 +1,4 @@
+from .core import Identical
+
+
+__all__ = ["Identical"]
---
+++ cfdata/tabular/processors/identical/core.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from ..base import Processor
+
+
+@Processor.register("identical")
+class Identical(Processor):
+    @property
+    def input_dim(self) -> int:
+        return 1
+
+    @property
+    def output_dim(self) -> int:
+        return 1
+
+    def fit(self,
+            columns: np.ndarray) -> Processor:
+        return self
+
+    def _process(self,
+                 columns: np.ndarray) -> np.ndarray:
+        return columns
+
+    def _recover(self,
+                 processed_columns: np.ndarray) -> np.ndarray:
+        return processed_columns
+
+
+__all__ = ["Identical"]
