0d281c650d813352a65b22bdeb48eab7ccfc086b
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-08-02 12:44:02 +0800

    在 cfdata.tabular 中暴露了 `DataTuple`

--- cfdata/tabular/__init__.py
+++ cfdata/tabular/__init__.py
@@ -4,7 +4,8 @@ from .wrapper import TabularData
 
 
 __all__ = [
-    "TabularData", "TabularDataset", "TaskTypes", "ColumnTypes",
+    "TabularData", "TabularDataset",
+    "TaskTypes", "ColumnTypes", "DataTuple",
     "SplitResult", "DataSplitter", "KFold", "KRandom",
     "ImbalancedSampler", "LabelCollators", "DataLoader"
 ]
