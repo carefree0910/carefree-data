6a31882d44b5341cb4f732b63329d60e6e7b38a9
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-08-26 04:39:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import numpy
 from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
 
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 DESCRIPTION = "Data processing module implemented with numpy"
 with open("README.md") as f:
