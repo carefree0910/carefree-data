20437594a319364ec6957b959599c77e7c203ed2
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-10-18 21:19:56 +0800

    更新了 carefree-toolkit 的依赖

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version=VERSION,
     packages=find_packages(exclude=("tests",)),
     install_requires=[
-        "carefree-toolkit>=0.1.4",
+        "carefree-toolkit>=0.2.0rc.3",
         "dill", "future", "psutil", "pillow",
         "cython>=0.29.12", "numpy>=1.16.2", "scipy>=1.2.1",
         "scikit-learn>=0.20.3", "matplotlib>=3.0.3",
