00bcd0122670a6eb768d54d50e1ca86e756aa2a6
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-02-26 10:49:36 +0800

    升级了 numpy 的依赖

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "psutil",
         "pillow",
         "cython>=0.29.12",
-        "numpy>=1.16.2",
+        "numpy>=1.20.0",
         "scipy>=1.2.1",
         "scikit-learn>=0.20.3",
         "matplotlib>=3.0.3",
